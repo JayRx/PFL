d600910d2adb76072fdd220e4791670f881dfec0
João Rocha <jr007silva1306@gmail.com>
2021-11-17 22:03:38 +0000

    Finished Exercise 1

--- Fib.hs
+++ Fib.hs
@@ -1,11 +1,21 @@
+-- Ex 1.1
 fibRec :: (Integral a) => a -> a
 fibRec 0 = 0
 fibRec 1 = 1
 fibRec a = fibRec (a - 1) + fibRec (a - 2)
 
+-- Ex 1.2
+fibLista :: Int -> Int
+fibLista a
+ | a == 0 = 0
+ | a == 1 = 1
+ | otherwise = (aux !! (a-1)) + (aux !! (a-2))
+    where
+      aux = map fibLista [0..]
 
-fibLista :: (Integral a) => a -> a
-fibLista a = a
+-- Ex 1.3
+fibs :: [Int]
+fibs = 0 : 1 : [a+b | (a,b)<-zip fibs (tail fibs)]
 
-fibListaInfinita :: (Integral a) => a -> a
-fibListaInfinita a = a
+fibListaInfinita :: Int -> Int
+fibListaInfinita a = last (take (a+1) fibs)
